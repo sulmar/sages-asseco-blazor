e17de145ed0cf903bbbf63baf0c18428873da11f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-14 16:32:44 +0200

    Dodanie projektu RealWorld

---
+++ src/RealWorld/Domain/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Models;
+
+public class Account : BaseEntity
+{
+    public string Number { get; set; }
+    public decimal Balance { get; set; }
+    public AccountStatus Status { get; set; }
+}
+
+
+public enum AccountStatus
+{
+    Opened,
+    Locked,
+    Closed
+}
