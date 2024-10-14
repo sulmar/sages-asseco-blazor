01d18398985acc4ce9df06a684941cb16c560c1f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-14 13:19:57 +0200

    Zastosowanie partial class

---
+++ src/Fundamentals/BlazorServerStaticApp/Components/Pages/Home.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorServerStaticApp.Components.Pages;
+
+public partial class Home
+{
+    private string message;
+
+    private void OnButtonClick()
+    {
+        message = "Clicked!";
+        Console.WriteLine(message);
+
+        // StateHasChanged();
+    }
+}
