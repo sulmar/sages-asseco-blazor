0125113e52a0c2151744f4a929e54ee0b67d8628
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-15 12:27:36 +0200

    Refaktor: Zastosowanie wyrazen lambda

--- src/RealWorld/Infrastructure/FakeAccountRepository.cs
+++ src/RealWorld/Infrastructure/FakeAccountRepository.cs
@@ -5,12 +5,9 @@ namespace Infrastructure;
 
 public class FakeAccountRepository : IAccountRepository
 {
-    private IDictionary<int, Account> _accounts;
+    private readonly IDictionary<int, Account> _accounts;
 
-    public FakeAccountRepository(IEnumerable<Account> accounts)
-    {
-        _accounts = accounts.ToDictionary(p => p.Id);
-    }
+    public FakeAccountRepository(IEnumerable<Account> accounts) => _accounts = accounts.ToDictionary(p => p.Id);
     public void Add(Account account)
     {
         var id = _accounts.Max(p=>p.Key);
@@ -20,13 +17,7 @@ public class FakeAccountRepository : IAccountRepository
         _accounts.Add(account.Id, account);
     }
 
-    public Account Get(int id)
-    {
-        return _accounts[id];
-    }
+    public Account Get(int id) => _accounts[id];
 
-    public IEnumerable<Account> GetAll()
-    {
-        return _accounts.Values;
-    }
+    public IEnumerable<Account> GetAll() => _accounts.Values;
 }
